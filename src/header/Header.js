74695ce66c96d4a6a306c6bc274b1c6c4be694fa
vickyrockingster <vrockingster@gmail.com>
2019-04-16 10:10:36 -0400

    Can successfully CRUD users table from the browser
    -I can successfully sign up, sign in, sign out, and change my password
    from the client

--- src/header/Header.js
+++ src/header/Header.js
@@ -1,33 +1,33 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { Link } from 'react-router-dom'
 
 import './Header.scss'
 
 const authenticatedOptions = (
-  <React.Fragment>
+  <Fragment>
     <Link to="/change-password">Change Password</Link>
     <Link to="/sign-out">Sign Out</Link>
-  </React.Fragment>
+  </Fragment>
 )
 
 const unauthenticatedOptions = (
-  <React.Fragment>
+  <Fragment>
     <Link to="/sign-up">Sign Up</Link>
     <Link to="/sign-in">Sign In</Link>
-  </React.Fragment>
+  </Fragment>
 )
 
 const alwaysOptions = (
-  <React.Fragment>
+  <Fragment>
     <Link to="/">Home</Link>
-  </React.Fragment>
+  </Fragment>
 )
 
 const Header = ({ user }) => (
   <header className="main-header">
-    <h1>Uber, But For Taxis</h1>
+    <h1>Scribe</h1>
     <nav>
-      { user && <span>Welcome, {user.email}</span>}
+      { user && <span>Welcome, {user.firstName}</span>}
       { user ? authenticatedOptions : unauthenticatedOptions }
       { alwaysOptions }
     </nav>
